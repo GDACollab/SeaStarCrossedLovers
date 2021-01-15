37df8d47be8f23827f5e80e0781fcbd69623e879
Evan Li <eli24@ucsc.edu>
2021-01-14 16:47:02 -0800

    Simple blocks spawn on click

---
+++ Sea Star Crossed Lovers/Assets/Scripts/SpawnBlockAtMouse.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnBlockAtMouse : MonoBehaviour
+{
+    public GameObject Block;
+    private Camera Camera;
+
+    private void Start()
+    {
+        Camera = Camera.main;
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("Left mouse button clicked");
+
+            Vector3 spawnPosition = Camera.ScreenToWorldPoint(Input.mousePosition);
+            spawnPosition.z = 0;
+
+            Instantiate(Block, spawnPosition, Quaternion.identity);
+
+            Debug.Log("Block spawnPosition: " + spawnPosition);
+        }
+    }
+}
