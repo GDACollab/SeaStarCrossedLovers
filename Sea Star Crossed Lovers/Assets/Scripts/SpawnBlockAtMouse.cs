27ef73666d5b5a84e86a0e065c51f26ef19640e5
Ben Paulsen <benpaulsen@MacBook-Pro.local>
2021-01-15 20:02:02 -0800

    starter placement and gravity controls

--- Sea Star Crossed Lovers/Assets/Scripts/SpawnBlockAtMouse.cs
+++ Sea Star Crossed Lovers/Assets/Scripts/SpawnBlockAtMouse.cs
@@ -5,7 +5,13 @@ using UnityEngine;
 public class SpawnBlockAtMouse : MonoBehaviour
 {
     public GameObject Block;
+    public float horizontalSpeed = 10;
+    public float dropSpeed = 1;
+    public int startingDownwardForce = 200;
+    
+    private float blockGravity;
     private Camera Camera;
+    private GameObject activeBlock;
 
     private void Start()
     {
@@ -14,16 +20,37 @@ public class SpawnBlockAtMouse : MonoBehaviour
 
     private void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0) && activeBlock == null)
         {
             Debug.Log("Left mouse button clicked");
 
             Vector3 spawnPosition = Camera.ScreenToWorldPoint(Input.mousePosition);
             spawnPosition.z = 0;
 
-            Instantiate(Block, spawnPosition, Quaternion.identity);
+            activeBlock = Instantiate(Block, spawnPosition, Quaternion.identity);
+            blockGravity = activeBlock.GetComponent<Rigidbody2D>().gravityScale;
+            activeBlock.GetComponent<Rigidbody2D>().gravityScale = 0f;
 
             Debug.Log("Block spawnPosition: " + spawnPosition);
         }
+
+        if (activeBlock != null) {
+            float xOffset = Input.GetAxisRaw("Horizontal")*Time.deltaTime*horizontalSpeed;
+            activeBlock.transform.position += new Vector3(xOffset, -dropSpeed*Time.deltaTime, 0);
+        
+            // detect if player is done with block
+            if (Input.GetButtonDown("Drop")) {
+                activeBlock.GetComponent<BlockCollision>().isActive = false;
+            }
+
+            // if an active block is no longer active, give it normal gravity
+            if (!activeBlock.GetComponent<BlockCollision>().isActive) {
+                activeBlock.GetComponent<Rigidbody2D>().gravityScale = blockGravity;
+                activeBlock.GetComponent<Rigidbody2D>().AddForce(new Vector3(0, -startingDownwardForce, 0));
+                activeBlock = null;
+            }
+        }
+
+
     }
 }
