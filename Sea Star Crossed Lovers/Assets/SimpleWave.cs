f30700a6fdf178eb1f2565b478860b6020db3ae6
CheeseAnddRice <hamawill@ucsc.edu>
2021-01-26 13:53:23 -0800

    More wave stuff, wave collisions with blocklets
    Waves now show up at a set interval and height
    Can collide and tell blocklets to delete themselves (will be handled by blocklets), with number of rows of blocks depending on rising "difficulty"
    Boolean waveIsOver is true when a wave has passed endpoint (should be safe to delete blocklets)

--- Sea Star Crossed Lovers/Assets/SimpleWave.cs
+++ Sea Star Crossed Lovers/Assets/SimpleWave.cs
@@ -4,7 +4,61 @@ using UnityEngine;
 
 public class SimpleWave : MonoBehaviour
 {
-    public void MakeWave() {
-        GetComponent<Waves>().GenerateWave(new DisruptiveWave());
+    public int difficulty; //how many rows of blocks to delete
+    public int maxDifficulty = 3;
+    public float waveCooldown = 10f;
+    public int waveHeight = 3;
+    public float timeBetweenDifficulties = 30f; //in seconds
+    private float wTimer; //wave timer
+    private float dTimer; //difficulty timer
+    public bool waveIsOver; //true if wave has passed, false if wave is currently passing through
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        difficulty = 1;
+        wTimer = 0f;
+        dTimer = 0f;
+        waveIsOver = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Adds 1 to difficulty if enough time has passed and difficulty isn't max
+        wTimer += Time.deltaTime;
+        dTimer += Time.deltaTime;
+        if (wTimer > waveCooldown)
+        {
+            MakeWave(waveHeight);
+            wTimer = 0f;
+        }
+        if (dTimer > timeBetweenDifficulties && difficulty < maxDifficulty)
+        {
+            difficulty++;
+            dTimer = 0f;
+        }
+    }
+
+    void OnParticleCollision(GameObject other)
+    {
+        if (other.tag == "Blocklet")
+        {
+            //    Blocklet b = other.GetComponent<Blocklet>();
+            //    b.Delete(difficulty);
+        }
+        else if (other.name == "Endpoint")
+            waveIsOver = true;
+    }
+
+    public void MakeWave()
+    {
+        MakeWave(1);
+    }
+
+    public void MakeWave(int height) {
+        waveIsOver = false;
+        for(int i = 0; i < height; i++)
+            GetComponent<Waves>().GenerateWave(new DisruptiveWave());
     }
 }
