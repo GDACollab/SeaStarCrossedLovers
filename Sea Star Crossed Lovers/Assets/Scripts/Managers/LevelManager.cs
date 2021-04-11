bd699a909d3e4197af32edbddf8c4f8c6a57bdb8
Evan Li <eli24@ucsc.edu>
2021-04-11 13:23:49 -0700

    File organization; added some comments

--- Sea Star Crossed Lovers/Assets/Scripts/Managers/LevelManager.cs
+++ Sea Star Crossed Lovers/Assets/Scripts/Managers/LevelManager.cs
@@ -25,6 +25,7 @@ public class LevelManager : MonoBehaviour
 
     private void Awake()
     {
+        // Getting all necessary references
         _blockManager = gameObject.GetComponent<BlockManager>();
         _blockManager.Construct(this);
 
@@ -42,6 +43,8 @@ public class LevelManager : MonoBehaviour
         if(currentGameState == GameState.playing &&
             _goalpoint.CheckWin() && !WinTimerOnCooldown)
         {
+            // In case the winTimer is already running
+            // (theoretically not possible?) stop it so it doesn't run twice
             StopCoroutine(winTimer());
             currentGameState = GameState.winTimer;
             StartCoroutine(winTimer());
@@ -53,17 +56,24 @@ public class LevelManager : MonoBehaviour
         winText.text = "Win timer: " + timeToWin;
 
         float currWaitTime = 0;
+        // Continuously check if the goalpoint detects the player
+        // is still elligible to win until the win timer reaches timeToWin
         while (currWaitTime < timeToWin)
         {
             if (_goalpoint.CheckWin())
             {
+                // Update text
                 float timeLeft = timeToWin - currWaitTime;
+                // Display timeLeft float with 1 decimal place
                 winText.text = "Win timer: " + timeLeft.ToString("F1");
+                // Wait increment amount of time for next update
                 currWaitTime += winTimerIncrement;
                 yield return new WaitForSeconds(winTimerIncrement);
             }
             else
             {
+                // If ever the player loses elligibility to win during
+                // the timer, reset text, exit win timer, and start cooldown
                 winText.text = defualtWinTimerMessage;
                 StartCoroutine(WinTimerCooldown());
                 currentGameState = GameState.playing;
@@ -71,22 +81,18 @@ public class LevelManager : MonoBehaviour
             }
         }
 
+        // If remain elligibile for win the whole time, play wins
         Debug.Log("You win");
         winText.text = "YOU WIN! You may continue playing.";
         currentGameState = GameState.won;
     }
 
+    // Used to stop Update function from starting winTimer in finicky situations
+    // where player is elligible and then unelligible in a short amount of time
     private IEnumerator WinTimerCooldown()
     {
         WinTimerOnCooldown = true;
         yield return new WaitForSeconds(winTimerCooldown);
         WinTimerOnCooldown = false;
-    }
-
-    public void ChangeScene(string SceneName)
-    {
-        // TODO insert some scene transition here
-
-        SceneManager.LoadScene(SceneName);
     }
 }
