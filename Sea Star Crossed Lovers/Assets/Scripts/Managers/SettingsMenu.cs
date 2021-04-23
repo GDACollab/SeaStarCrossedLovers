addaebe7781b6eaeb2097c219061593e195827e2
Ben Paulsen <benpaulsen@MacBook-Pro.local>
2021-04-23 14:58:12 -0700

    basic menu and partial pausing

---
+++ Sea Star Crossed Lovers/Assets/Scripts/Managers/SettingsMenu.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SettingsMenu : MonoBehaviour
+{
+    public void toggleSettingsMenu() 
+    {
+        bool paused = gameObject.activeSelf;
+
+        // Reverse the active setting
+        gameObject.SetActive(!paused);
+
+        // Pause/Unpause physics
+        if (paused)
+            Time.timeScale = 1;
+        else
+            Time.timeScale = 0;
+    }
+}
